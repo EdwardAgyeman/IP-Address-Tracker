6408e3329376840fa015f315106317a88e6b7585
eddicodes <edw.agyeman@gmail.com>
2020-10-27 13:34:45 +0100

    IP Address on the map

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,4 @@
-let userInput = document.querySelector("input");
-let button = document.querySelector("button");
-// let myIP = "192.168.0.24"
-// let currentIP;
-
-button.addEventListener("mouseover", function() { //click is not working
-    // pass the userInput value to fetch &ipAddress
-    // currentIP = userInput.value
-    document.querySelector("h1").textContent = userInput.value;
-})
-
-fetch(`https://geo.ipify.org/api/v1?apiKey=at_nSwsJjV340Nc9JurI3BAWK8Lq5uJ9&ipAddress=${userInput.value}`) //${currentIP} not working ... ${userInput.value} is static
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        let ipAddress = document.querySelector("#ip-address");
-        let loCation = document.querySelector("#location");
-        let timezone = document.querySelector("#timezone");
-        // let utc = document.querySelector("#utc");
-        let isp = document.querySelector("#isp");
-
-        // pass data to the DOM Elements
-        ipAddress.textContent = data.ip;
-        loCation.textContent = data.location.city;
-        timezone.textContent = data.location.timezone;
-        isp.textContent = data.isp;
-    })
-    .catch(error => console.log("ERROR! Something went wrong!"))
-
-
 // ========================================Map Set Up========================================
-
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWRkaWNvZGVzIiwiYSI6ImNrZ3JyMmo1cTA0bjQyeW1uYXhzN3R5Z3MifQ.mBkRmRfBgRJx_GneLxPSnA', {
@@ -41,8 +10,33 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
 
-// function onMapClick(e) {
-//     alert("You clicked the map at " + e.latlng);
-// }
 
-// mymap.on('click', onMapClick);
+
+let userInput = document.querySelector("input");
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    // add validation ip address format required using reg ex "Please enter valid IP" CSS hidden
+    fetch(`https://geo.ipify.org/api/v1?apiKey=at_nSwsJjV340Nc9JurI3BAWK8Lq5uJ9&ipAddress=${userInput.value}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            let ipAddress = document.querySelector("#ip-address");
+            let loCation = document.querySelector("#location");
+            let timezone = document.querySelector("#timezone");
+            // let utc = document.querySelector("#utc");
+            let isp = document.querySelector("#isp");
+
+            // pass data to the DOM Elements
+            ipAddress.textContent = data.ip;
+            loCation.textContent = data.location.city;
+            timezone.textContent = data.location.timezone;
+            isp.textContent = data.isp;
+
+            mymap.setView([data.location.lat, data.location.lng], 13)
+        })
+        .catch(error => console.log("ERROR! Something went wrong!"))
+})
+console.log(mymap)
+console.log(L)
