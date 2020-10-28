0509703cdce14a799cfe030ce62e622e3906e481
eddicodes <edw.agyeman@gmail.com>
2020-10-28 09:57:38 +0100

    Added Marker to Map

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 // ========================================Map Set Up========================================
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWRkaWNvZGVzIiwiYSI6ImNrZ3JyMmo1cTA0bjQyeW1uYXhzN3R5Z3MifQ.mBkRmRfBgRJx_GneLxPSnA', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -9,8 +10,6 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
-
-
 
 let userInput = document.querySelector("input");
 let form = document.querySelector("form");
@@ -35,8 +34,10 @@ form.addEventListener("submit", function(event) {
             isp.textContent = data.isp;
 
             mymap.setView([data.location.lat, data.location.lng], 13)
+            marker = L.marker([data.location.lat, data.location.lng]).addTo(mymap);
         })
         .catch(error => console.log("ERROR! Something went wrong!"))
 })
+
 console.log(mymap)
 console.log(L)
